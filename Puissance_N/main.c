ae110989adf111af7b1f3b7e581e101e434edb40
Leonard-hub-wq <83727577+Leonard-hub-wq@users.noreply.github.com>
2021-05-08 20:25:40 +0200

    show_grid  Grid
    ajout de ma fonction show_grid et de la structure Grid

--- Puissance_N/main.c
+++ Puissance_N/main.c
@@ -5,6 +5,14 @@
 #include "fonctions.h"  //on inclut la librairie
 #include "fonctions.c"
 
+typedef struct{
+    int hauteur;
+    int largeur;
+    char** grille;
+}Grid;
+
+void show_grid(char** grille,int lareur,int hauteur);
+
 int main(int argc, char* argv[]) {
 logo();
 char choice;
@@ -37,7 +45,32 @@ char choice;
 
         } while (choice!='S'&& choice!='N'&& choice!='Q');
 
+printf(" ____                                                          \n|    |                                                |       |\n|____|                                                | |     |\n|         .   ___    ___   ___    ___    ___   ___    |   |   |\n|  |   |  |  |___   |___  |___|  |   |  |     |___    |     | |\n|  |___|  |   ___|   ___| |   |  |   |  |___  |___    |       |\n");
+    int N;
+    Grid g1;
+    printf("entrer la valeur de N+2:\n");
+    scanf("%d",&N);
+    g1.largeur = N;
+    g1.hauteur = N;
+
+    g1.grille = (char**) malloc(sizeof (char*)*g1.largeur);
+    for (int i=0;i<g1.largeur;i++){
+        g1.grille[i]=(char*) malloc(sizeof (char)*g1.hauteur);
+        for (int j=0;j<g1.hauteur;j++){
+            g1.grille[i][j]='_';
+        }
+    }
+
+    show_grid(g1.grille,g1.largeur,g1.hauteur);
 
 
 return EXIT_SUCCESS;
 }
+
+void show_grid(char** grille,int largeur, int hauteur){
+    for (int i=0;i<largeur;i++){
+        for (int j=0;j<hauteur;j++){
+            printf("{%c}\n",grille[i][j]);
+        }
+    }
+}//fonction show grid a changer car le printf ne renvoie pas sur forme d'un tableau
