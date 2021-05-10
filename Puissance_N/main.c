071192af5598c31b354a06166d80bb4ac1e4835e
Leonard-hub-wq <83727577+Leonard-hub-wq@users.noreply.github.com>
2021-05-10 18:49:13 +0200

    Update main.c

--- Puissance_N/main.c
+++ Puissance_N/main.c
@@ -45,12 +45,17 @@ char choice;
 
         } while (choice!='S'&& choice!='N'&& choice!='Q');
     
-      int N;
+    int N;
     Grid g1;
-    printf("entrer la valeur de N+2:\n");
+    printf("entrer la valeur de N:\n");
     scanf("%d",&N);
-    g1.largeur = N;
-    g1.hauteur = N;
+    while(N<0){
+        printf("erreur, N doit etre un entier positif!!\n");
+        printf("entrer la valeur de N:\n");
+        scanf("%d",&N);
+    }
+    g1.largeur = N+2;
+    g1.hauteur = N+2;
 
     g1.grille = (char) malloc(sizeof (char)g1.largeur);
     for (int i=0;i<g1.largeur;i++){
